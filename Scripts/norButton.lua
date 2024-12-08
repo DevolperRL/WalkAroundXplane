2ddebe9329ea0f5e8503066c374c24513eb441ff
DevolperRL <100154298+DevolperRL@users.noreply.github.com>
2024-12-08 11:45:52 +0100

    Delete Scripts directory

--- Scripts/norButton.lua
+++
@@ -1,9 +0,0 @@
-function onMouseDown()
-    get(click)
-    return true
-end
-
-function draw()
-    drawRectangle(0, 0, size[1], size[2], get(color))
-    drawText(get(font), size[1]/2, size[2]/2-5, get(text), get(fontSize), false, false, TEXT_ALIGN_CENTER, {1,1,1,1})
-end
